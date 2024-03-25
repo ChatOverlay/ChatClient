0158c46ea5237eecd25f7fa6ac782e8a208c01a1
Xingu <ojyoung2490@gmail.com>
2024-03-25 20:50:14 +0900

    Dev : 해당 질문 작성하게 설정

--- src/components/navbarlist/QuestionList.jsx
+++ src/components/navbarlist/QuestionList.jsx
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ListBox.css";
 import SelectLabels from "./select/SelectLabels";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import styled from "styled-components";
+
 export default function QuestionListBox() {
   const navigate = useNavigate();
   const [options, setOptions] = useState("전체 보기");
-  const questionList = ["Question1", "Question2"]; // Example question rooms
+  const questionList = [
+    { id: 1, title: "Question1", subTitle: "어려운 시대" },
+    { id: 2, title: "Question2", subTitle: "희망의 메시지" }, // Note the title change here for clarity
+  ]; // Example question rooms
 
   return (
     <div className="navbar__list">
@@ -13,12 +19,51 @@ export default function QuestionListBox() {
       {questionList.map((room) => (
         <div
           className="navbar__list__item"
-          key={room}
-          onClick={() => navigate(`/question/${room}`)}
+          key={room.id} // Use the room's id as the key
+          onClick={() => navigate(`/question/${room.id}`)} // Navigate using room's id
         >
-          {room}
+          <QuestionTitle>
+            <div>{room.title}</div>
+            <SubTitleContainer>{room.subTitle}</SubTitleContainer>
+          </QuestionTitle>
         </div>
       ))}
+      <IconContainer>
+        <DriveFileRenameOutlineIcon sx={{ color: "#202c39" }} />
+      </IconContainer>
     </div>
   );
 }
+
+const QuestionTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const SubTitleContainer = styled.div`
+  padding-top: 0.3rem;
+  font-weight: normal;
+  font-size: 0.8rem;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 1.5rem;
+  right : 1.5rem;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  color: #202c39;
+  background-color: #f2d492;
+  border-radius: 20rem;
+  
+  height : 3rem;
+  width : 3rem;
+  cursor: pointer;
+  transition : all 0.3s;
+  &:hover {
+    opacity : 0.6;
+  }
+`;
