7f8294d0d24397b5e4b6f22fa8344569da4941f2
Xingu <ojyoung2490@gmail.com>
2024-03-25 16:50:09 +0900

    Dev : 선택할 수 있는 Label 추가 및 css 설정

---
+++ src/components/navbarlist/select/SelectLabels.jsx
@@ -0,0 +1,42 @@
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function SelectLabels({ options, setOptions }) {
+  const handleChange = (event) => {
+    setOptions(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl
+        sx={{
+          m: 1,
+          minWidth: "20rem",
+          color: "white",
+          fontWeight: "bold",
+          marginTop: "0.1rem",
+          ".MuiInputLabel-root": { color: "#f2d492" }, // label color
+          ".MuiOutlinedInput-root": {
+            "& fieldset": { borderColor: "#f2d492" }, // border color
+            "&:hover fieldset": { borderColor: "#f2d492" }, // hover border color
+            "&.Mui-focused fieldset": { borderColor: "#f2d492" }, // focus border color
+          },
+          ".MuiInputBase-input": { color: "white" },
+
+          ".MuiSvgIcon-root": { color: "#f2d492" }, // Dropdown arrow color
+        }}
+      >
+        <Select
+          value={options}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem value={10}>전체보기</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
