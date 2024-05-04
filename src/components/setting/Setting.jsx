eb4c30f906d0774c765711159b72e585f0ae3ead
Xingu <ojyoung2490@gmail.com>
2024-05-04 18:30:43 +0900

    fix : setting 버그 수정

--- src/components/setting/Setting.jsx
+++ src/components/setting/Setting.jsx
@@ -73,7 +73,7 @@ const SettingContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
-  z-index: 10;
+  z-index: 1000;
 `;
 
 const SettingBox = styled.div`
