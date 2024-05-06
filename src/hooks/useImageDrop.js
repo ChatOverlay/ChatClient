0a0b103b2cc81a5e47845c8ec55932ba78317269
Xingu <ojyoung2490@gmail.com>
2024-05-06 23:24:16 +0900

    fix : skipped 6 이런 거 warning 뜨는 것 해결

--- src/hooks/useImageDrop.js
+++ src/hooks/useImageDrop.js
@@ -3,30 +3,22 @@ import { useCallback } from "react";
 export function useImageDrop(setPreviews, setImages) {
   return useCallback(
     (acceptedFiles) => {
-      const supportedExtensions = ["jpg", "jpeg", "png", "gif"]; // 지원하는 파일 확장자 정의
-      const newImages = [];
-      const newPreviews = [];
-
+      const supportedExtensions = ["jpg", "jpeg", "png", "gif"];
       acceptedFiles.forEach((file) => {
         const fileExtension = file.name.split(".").pop().toLowerCase();
-        if (
-          file.type.startsWith("image/") &&
-          supportedExtensions.includes(fileExtension)
-        ) {
+        if (supportedExtensions.includes(fileExtension)) {
           const reader = new FileReader();
           reader.onloadend = () => {
-            newPreviews.push(reader.result);
-            setPreviews((prev) => [...prev, ...newPreviews]);
+            setPreviews(prev => [...prev, reader.result]);
+            setImages(prev => [...prev, file]);
           };
           reader.readAsDataURL(file);
-          newImages.push(file);
-
-          setImages((prev) => [...prev, ...newImages]);
         } else {
-          alert(`${fileExtension}은 지원하지 않는 확장명입니다.`);
+          alert(`${fileExtension} 파일 확장자는 지원하지 않습니다.`);
         }
       });
     },
     [setPreviews, setImages]
   );
 }
+
