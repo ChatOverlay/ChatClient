25bbca6b44db7969207e0c55f62fe505d204a2e6
Xingu <ojyoung2490@gmail.com>
2024-05-19 02:39:40 +0900

    fix : 핸드폰 로그인 페이지 드래그 가능한 것 못하게 처리

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -3,26 +3,31 @@ import styled from "styled-components";
 import Login from "../../components/login/Login";
 import useIsAuth from "../../hooks/useIsAuth";
 import { useTheme } from "../../context/ThemeContext";
-import BackgroundImg from "../../assets/backgroundImg/sky.png"
+import BackgroundImg from "../../assets/backgroundImg/sky.png";
+
 export default function LoginPage() {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   useIsAuth();
+
   return (
-    <HomeContainer >
+    <HomeContainer>
       <Login />
     </HomeContainer>
   );
 }
+
 const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  position: relative;
+  width: 100vw; /* Ensure the container covers the entire viewport width */
+  position: fixed; /* Fix the position to prevent scrolling */
+  top: 0;
+  left: 0;
   z-index: 10000;
-  background-attachment: fixed; // Helps to fix background in place
-  background: url(${BackgroundImg}) no-repeat center center fixed; // Use the background image
-  background-size: cover; // Ensure the background covers the entire container
+  background: url(${BackgroundImg}) no-repeat center center fixed; /* Fix the background image */
+  background-size: cover; /* Ensure the background covers the entire container */
+  overflow: hidden; /* Prevent scrolling */
 `;
-
