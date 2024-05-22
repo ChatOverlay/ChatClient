a20f4b1aafb3246bf79eb8cdf508f1c37a81a112
XinguOh <ojyoung2490@gmail.com>
2024-05-22 14:30:44 +0900

    Dev : splash 화면 추가

--- vite.config.js
+++ vite.config.js
@@ -6,54 +6,63 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      registerType: "autoUpdate",
+      includeAssets: [
+        "icons/icon-512x512.png", // 사용하고자 하는 splash 이미지
+      ],
       manifest: {
-        name: '클라톡',
-        short_name: 'Clatalk',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#0164D8',
+        name: "클라톡",
+        short_name: "Clatalk",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0164D8",
         icons: [
           {
-            src: 'icons/icon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png'
+            src: "icons/icon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png'
+            src: "icons/icon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png'
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-196x196.png',
-            sizes: '196x196',
-            type: 'image/png'
+            src: "icons/icon-196x196.png",
+            sizes: "196x196",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-312x312.png',
-            sizes: '312x312',
-            type: 'image/png'
+            src: "icons/icon-312x312.png",
+            sizes: "312x312",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
-  ]
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        splash_pages: [
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "2048x2732", // 고해상도로 한 장의 이미지 사용
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
