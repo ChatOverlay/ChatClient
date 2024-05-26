2585dc9b42c7880ba5e5ad394c5bc26eb9adf229
Xingu <ojyoung2490@gmail.com>
2024-05-26 12:15:35 +0900

    fix : splash screen 및 새로고침 방지했던 거 제거

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const PreventPullToRefresh = ({ children }) => {
-  useEffect(() => {
-    const preventPullToRefresh = (e) => {
-      if (e.touches.length > 1) {
-        e.preventDefault();
-      }
-    };
-
-    document.addEventListener('touchmove', preventPullToRefresh, { passive: false });
-
-    return () => {
-      document.removeEventListener('touchmove', preventPullToRefresh);
-    };
-  }, []);
-
-  return children;
-};
-
 root.render(
   <React.StrictMode>
-    <PreventPullToRefresh>
       <App />
-    </PreventPullToRefresh>
   </React.StrictMode>
 );
