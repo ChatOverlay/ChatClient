0c10f2f6b9d5552597be5f40618405887b62994e
Xingu <ojyoung2490@gmail.com>
2024-06-02 16:25:11 +0900

    Design : 질문 컨테이너 디자인 수정

--- src/components/navbarlist/questiongrid/QuestionGrid.jsx
+++ src/components/navbarlist/questiongrid/QuestionGrid.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import QuestionContent from "../../question/QuestionContent";
 import Questioner from "../../question/Questioner";
 import { useTheme } from "../../../context/ThemeContext";
 import styled from "styled-components";
 import "../ListBox.css";
 
-export default function QuestionGrid({ questionList, editMode, setEditMode,onCommentClick}) {
+export default function QuestionGrid({
+  questionList,
+  editMode,
+  setEditMode,
+  onCommentClick,
+}) {
   const { theme } = useTheme();
-   return (
+
+ 
+  return (
     <div className="grid__list">
       {questionList.map((question) => (
         <div key={question._id}>
@@ -36,7 +43,6 @@ const CourseName = styled.div`
   display: flex;
   padding: 0.5rem 0 0.5rem 1rem;
   font-size: 1.5rem;
-  border-top : 1px solid var(--foreground-color);
   color: var(--foreground-color);
   margin-bottom: -3rem;
   &:first-child {
